06b521af084c6eb5f00d108c2a147fb28ceff539
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2025-09-10 19:01:52 +0530

    change Navbar hover color

--- app/components/NavbarFavorites.js
+++ app/components/NavbarFavorites.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useFavorites } from "../context/FavoritesContext"; // path adjust
+import { useFavorites } from "../context/FavoritesContext"; 
 import Link from "next/link";
 import { FiHeart } from "react-icons/fi";
 
@@ -8,7 +8,7 @@ export default function NavbarFavorites() {
   const { favorites } = useFavorites();
 
   return (
-    <Link href="/favorites" className="relative text-white hover:text-red-500">
+    <Link href="/favorites" className="relative text-white hover:text-black">
       <FiHeart size={24} />
       {favorites.length > 0 && (
         <span className="absolute -top-2 -right-2 bg-red-500 rounded-full text-xs w-5 h-5 flex items-center justify-center text-white font-bold">
